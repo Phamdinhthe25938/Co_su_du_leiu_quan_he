e00159810b2bd1e4eee33986b10646f092a5805f
Phamdinhthe250938 <phamdinhthe2509@gmail.com>
2022-06-17 08:31:32 +0700

    add

---
+++ thao_tac_sql.sql
@@ -0,0 +1,11 @@
+
+create table c0322g1.student(
+    id int,
+    name varchar(255),
+    age int,
+    address varchar(255),
+    primary key (id)
+);
+insert into c0322g1.student values(1,'The',20,'trung lao');
+insert into c0322g1.student values(2,'nam',22,'trung lao');
+drop table c0322g1.student ;
